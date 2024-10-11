2f9b1f62fb7ce7e6c6fac0b5694738254cbd9d28
Muaz Khalid <itzmajo786@gmail.com>
2024-10-11 15:59:03 +0500

    Starting Advance Feature

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -2,18 +2,11 @@ import React from "react";
 import { motion } from "framer-motion";
 const FeatureCard = ({ Icon, heading, detail }) => {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 1 }}
-      className="bg-white shadow-lg  rounded-lg hover:bg-primary hover:text-white transform duration-500 ease-in-out hover:-translate-y-4 md:w-1/2 lg:w-1/4 cursor-pointer group flex flex-col justify-center items-center py-5 gap-2"
-    >
-      <div className="flex flex-col justify-center px-2 py-10 items-center">
-        <Icon className="text-6xl duration-500 ease-in-out group-hover:text-white text-blue-500" />
-        <p className="text-xl my-3 font-semibold text-center">{heading}</p>
-        <p className="text-center">{detail}</p>
-      </div>
-    </motion.div>
+    <div className="flex shadow-xl  duration-500 ease-in-out hover:bg-primary group hover:text-white flex-col justify-center px-2 py-20 items-center">
+      <Icon className="text-6xl duration-500 ease-in-out group-hover:text-white text-blue-500" />
+      <p className="text-xl my-3 font-semibold text-center">{heading}</p>
+      <p className="text-center">{detail}</p>
+    </div>
   );
 };
 
--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -9,7 +9,7 @@ const Offers = () => {
   return (
     <div className="p-s flex flex-col lg:flex-row gap-10">
       <div className="flex container flex-col lg:flex-row justify-center gap-10">
-        <InView classID="hover:-translate-y-4" triggerOnce={true} threshold={0.1}>
+        <InView triggerOnce={true} threshold={0.01}>
           {({ inView, ref }) => (
             <motion.div
               ref={ref}
@@ -25,7 +25,9 @@ const Offers = () => {
                     Custom Development Solution
                   </p>
                   <p className="text-center">
-                  Our developers craft custom software solutions by leveraging cutting- edge technologies, like blockchain and AI, to streamline workflow and relaibility.
+                    Our developers craft custom software solutions by leveraging
+                    cutting- edge technologies, like blockchain and AI, to
+                    streamline workflow and relaibility.
                   </p>
                 </div>
               </div>
@@ -33,14 +35,14 @@ const Offers = () => {
           )}
         </InView>
 
-        <InView triggerOnce={true} threshold={0.1}>
+        <InView triggerOnce={true} threshold={0.01}>
           {({ inView, ref }) => (
             <motion.div
               ref={ref}
               className="w-full lg:w-1/3"
               initial={{ opacity: 0, y: 200 }}
               animate={inView ? { opacity: 1, y: 0 } : {}}
-              transition={{ duration: 0.5, delay: 0.5 }}
+              transition={{ duration: 0.5, delay: 0.3 }}
             >
               <div className="bg-light rounded-lg hover:bg-primary hover:text-white transform duration-500 ease-in-out cursor-pointer p-10 flex flex-col justify-between items-center gap-5 h-full  hover:-translate-y-4 ">
                 <div className="flex flex-col items-center">
@@ -60,14 +62,14 @@ const Offers = () => {
           )}
         </InView>
 
-        <InView triggerOnce={true} threshold={0.1}>
+        <InView triggerOnce={true} threshold={0.01}>
           {({ inView, ref }) => (
             <motion.div
               ref={ref}
               className="w-full lg:w-1/3"
               initial={{ opacity: 0, y: 200 }}
               animate={inView ? { opacity: 1, y: 0 } : {}}
-              transition={{ duration: 0.5, delay: 1 }}
+              transition={{ duration: 0.5, delay: 0.5 }}
             >
               <div className="bg-light rounded-lg hover:bg-primary hover:text-white transform duration-500 ease-in-out cursor-pointer p-10 flex flex-col justify-between items-center gap-5 h-full  hover:-translate-y-4 ">
                 <div className="flex flex-col items-center">
@@ -76,7 +78,9 @@ const Offers = () => {
                     Courses We Offer
                   </p>
                   <p className="text-center">
-                  We offer differnet courses which are based on programming languange. like web-development, web-designing, App-development, Phython, SQL, MERNSTACK.
+                    We offer differnet courses which are based on programming
+                    languange. like web-development, web-designing,
+                    App-development, Phython, SQL, MERNSTACK.
                   </p>
                 </div>
               </div>
