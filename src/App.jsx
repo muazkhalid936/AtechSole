9a1949b471ae47e757cfd5e4bd5cad0c7d18d330
Muaz Khalid <itzmajo786@gmail.com>
2024-10-12 15:25:33 +0500

    Starting services page

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import About from "./pages/About";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollToTopButton from "./components/ScrollToTopButton";
+import Services from "./components/Services";
+import Hero from "./components/Hero";
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
+        <Route path="/Services" element={<Services />} />
       </Routes>
       <Footer />
       <ScrollToTopButton />
